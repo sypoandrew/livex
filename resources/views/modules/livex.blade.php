cd935ee5a723e264b8849c3abed1281524605d81
sypoandrew <62023239+sypoandrew@users.noreply.github.com>
2020-03-11 16:27:44 +0000

    Update livex.blade.php

--- resources/views/modules/livex.blade.php
+++ resources/views/modules/livex.blade.php
@@ -7,25 +7,6 @@
     @include('admin::partials.alerts')
 	<form action="{{ route('admin.modules.livex') }}" method="post" class="flex flex-wrap">
 		<div class="card mt-4 w-full">
-			<h3>Duty-paid rate settings</h3>
-			<div class="mt-4 w-full">
-			<label for="enabled" class="block">Rates per X litre</label>
-			<input type="text" id="litre_calc" name="litre_calc" autocomplete="off" required="required" class="w-full " value="{{ setting('Livex.litre_calc') }}">
-			</div>
-			<!--<div class="mt-4 w-full">
-			<label for="enabled" class="block">Still Wine Rate</label>
-			<input type="text" id="still_wine_rate" name="still_wine_rate" autocomplete="off" required="required" class="w-full " value="{{ setting('Livex.still_wine_rate') }}">
-			</div>
-			<div class="mt-4 w-full">
-			<label for="enabled" class="block">Sparkling Wine Rate</label>
-			<input type="text" id="sparkling_wine_rate" name="sparkling_wine_rate" autocomplete="off" required="required" class="w-full " value="{{ setting('Livex.sparkling_wine_rate') }}">
-			</div>
-			<div class="mt-4 w-full">
-			<label for="enabled" class="block">Fortified Wine Rate</label>
-			<input type="text" id="fortified_wine_rate" name="fortified_wine_rate" autocomplete="off" required="required" class="w-full " value="{{ setting('Livex.fortified_wine_rate') }}">
-			</div>-->
-		</div>
-		<div class="card mt-4 w-full">
 			<h3>Liv-Ex API settings for Aero Commerce</h3>
 			<div class="mt-4 w-full">
 			<label for="enabled" class="block">Enabled</label>
